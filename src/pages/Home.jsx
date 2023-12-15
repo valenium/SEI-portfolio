8a55d6eca693aa6e46d2016ee405ce79617e9f75
valenium <valenium@live.com>
2023-12-14 21:06:32 -0600

    image paths edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home(props) {
             <h1 className="text-pink-200 font-bebas text-9xl text-center">Valerie Lim</h1>
         </div>
         <div>
-            <img src="/src/assets/me.png" alt="Animated photo of Valerie"/>
+            <img src="/public/me.png" alt="Animated photo of Valerie"/>
         </div>
         <div className="flex justify-center flex-col pr-10">
             <h2 className="p-4 text-white font-oxygen text-2xl text-right">Full Stack Developer</h2>
