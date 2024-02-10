99e2e7e8181c2aec6b246a7cd9560264fb5f0656
valenium <valenium@live.com>
2024-02-10 10:21:23 -0800

    abou text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,45 +2,74 @@ import { useState, useEffect } from "react";
 import "./About.css";
 
 function About(props) {
-    const [about, setAbout] = useState(null)
+  const [about, setAbout] = useState(null);
 
-    const getAboutData = async () => {
-        const response = await fetch("./about.json")
-        const data = await response.json()
-        setAbout(data)
-    }
+  const getAboutData = async () => {
+    const response = await fetch("./about.json");
+    const data = await response.json();
+    setAbout(data);
+  };
 
-    useEffect(() => { getAboutData() }, [])
+  useEffect(() => {
+    getAboutData();
+  }, []);
 
-    const loaded = () => (
-        <div className="about px-20 py-6">
-            <div>
-                <h1 className="text-pink-200 font-bebas text-6xl">About Me</h1>
-                <h3 className="text-white font-oxygen text-xl">{about.bio}</h3>
-            </div>
+  const loaded = () => (
+    <div className="about">
+      <h1 className="text-pink-200 font-bebas text-6xl text-center">
+        About Me
+      </h1>
+      <div className="about-text">
+        <p>
+          I am a <strong>Full-Stack Software Engineer</strong> with a diverse
+          background in{" "}
+          <strong>
+            product management, customer success, project management,
+            operations,
+          </strong>{" "}
+          and <strong>management</strong>. After spending 3 years working in
+          gaming and cybersecurity, I decided to become more technical after
+          realizing I was much more interested in development. My unique blend of developer experience combined with my
+          obsession with organization and efficiency allow me to bridge the gap
+          between technical and non-technical stakeholders.
+        </p>
+        <br />
+        <p>
+          I've had the opportunity to lead the development of features on FC 24
+          such as the main menu redesign and closed beta, where I organized and
+          collaborated with large multidiscplinary and cross-functional teams
+          throughout the organization. My experience spans to helping build a
+          team from the ground up at an early stage cybersecurity startup, where
+          I had a strong influence on product design, customer success, and
+          operational processes.{" "}
+          <strong>
+            Now that I have graduated from a Full Stack Development bootcamp, I
+            am ready and excited to bring my ambition and drive into building
+            meaningful solutions.
+          </strong>
+        </p>
+        <br />
+        <p>
+          I have been consistently awarded for{" "}
+          <strong>championing positive work culture and positivity</strong>. One
+          of the most important things to me is collaborating in a meaningful
+          and thoughtful way with my peers, in and out of the workplace. I truly
+          believe that the positivity and energy I bring into my work enables me
+          to be a passionate contributor that promotes the same energy from my
+          team.
+        </p>
+        <br />
+        <p>
+          On a more personal note, I am extremely passionate about issues
+          related to sustainability and self-determination. I am also an avid
+          gamer, amateur baker, fitness junkie, and mother to an extremely
+          stubborn dog.
+        </p>
+      </div>
+    </div>
+  );
 
-            <div className="w-52">
-                <h2 className="font-oxygen text-white text-3xl pb-5 font-semibold">Hobbies</h2>
-                <ul>
-                {
-                    about.hobbies.map((hobby, idx) =>(
-                        <div key={idx}>
-                            <li className="flex justify-around font-oxygen text-white text-xl"><img src={hobby.icon} alt={hobby.hobby}/>
-                            <div className="text-start w-11">
-                                {hobby.hobby}
-                            </div>
-                            </li>
-                        </div>
-                    ))
-                }
-                </ul>
-            </div>
-
-        </div>
-    )
-
-    return about ? loaded(): <h1>Loading...</h1>;
+  return about ? loaded() : <h1>Loading...</h1>;
 }
-  
-  export default About;
-  
+
+export default About;
