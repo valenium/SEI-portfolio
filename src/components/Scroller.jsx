99fe5a566231f4f8a5eb0c762f5b1b586424fb46
valenium <valenium@live.com>
2024-02-14 10:27:55 -0800

    adjust scroller time

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -6,7 +6,7 @@ const Scroller = ({ texts }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    }, 3000);
+    }, 2000);
 
     return () => clearTimeout(timer); // Cleanup the timer when the component unmounts or before setting a new timer
   }, [currentIndex, texts.length]); // Dependencies array, effect runs when these values change
